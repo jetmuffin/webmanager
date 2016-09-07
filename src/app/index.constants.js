23f39a051d820c82f8389efd14d39158b5c2e625
JetMuffin <564936642@qq.com>
2016-09-07 14:39:43 +0800

    Generate basic architecture by yeoman
    Signed-off-by: JetMuffin <564936642@qq.com>

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('webmanager')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
