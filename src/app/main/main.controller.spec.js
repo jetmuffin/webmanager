0bea77b52e894425f0c04736f12e457112e4295a
JetMuffin <564936642@qq.com>
2016-09-11 14:26:35 +0800

    Fix test error
    Signed-off-by: JetMuffin <564936642@qq.com>

--- src/app/main/main.controller.spec.js
+++ src/app/main/main.controller.spec.js
@@ -4,21 +4,8 @@
     describe('controllers', function(){
 
         // FIXME add test cases
-        // example:
-        // var vm;
-        // beforeEach(module('webmanager'));
-        // beforeEach(inject(function(_$controller_, _$timeout_, _webDevTec_, _toastr_) {
-        //     spyOn(_webDevTec_, 'getTec').and.returnValue([{}, {}, {}, {}, {}]);
-        //     spyOn(_toastr_, 'info').and.callThrough();
-        //
-        //     vm = _$controller_('MainController');
-        //     $timeout = _$timeout_;
-        //     toastr = _toastr_;
-        // }));
-        //
-        // it('should have a timestamp creation date', function() {
-        //     expect(vm.creationDate).toEqual(jasmine.any(Number));
-        // });
+        it('example test case', function() {
+        });
 
     });
 })();
